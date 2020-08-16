f44017e9b19d824aa426e2fa8d32d2a2058b447e
ELLIOTT <eliott.chkayben@gmail.com>
2020-08-16 16:20:52 -0400

    App center integrations

--- RetirementCalculator/ViewController.swift
+++ RetirementCalculator/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import AppCenterCrashes
+import AppCenterAnalytics
 
 class ViewController: UIViewController {
+    @IBOutlet weak var monthlyInvestmenentTextField: UITextField!
+    @IBOutlet weak var currentAgeTextField: UITextField!
+    @IBOutlet weak var retirementAgeTextField: UITextField!
+    @IBOutlet weak var interestRateTextField: UITextField!
+    @IBOutlet weak var savingsTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if MSCrashes.hasCrashedInLastSession() {
+            let alert = UIAlertController(title: "Oops", message: "Sorry about that, an error occured", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "It's cool", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
 
+        MSAnalytics.trackEvent("navigate_to_calculator")
+    }
+
+    @IBAction func calculateButtonClicked(_ sender: Any) {
+        //MSCrashes.generateTestCrash()
+        guard let currentAge = Int(self.currentAgeTextField.text!) else { return }
+        guard let plannedRetirementAge = Int(self.retirementAgeTextField.text!) else { return }
+        let propreties = ["current_age": String(currentAge),
+                          "planned_retirement_age": String(plannedRetirementAge)]
+        MSAnalytics.trackEvent("calculate_retirement_amount", withProperties: propreties)
+    }
 }
 
